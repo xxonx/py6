f7112245971b2d5dbf6c4d818901238d8eaec15a
Matthias Wagner <xon.wagner@yahoo.de>
2015-08-03 22:22:29 +0200

    added collision handling
    handling method, collision object

--- src/collision_detection.py
+++ src/collision_detection.py
@@ -2,6 +2,8 @@
 
 """ Handles collision detection """
 
+from vec2d import Vec2D
+
 from box import Box
 from circle import Circle
 
@@ -59,3 +61,73 @@ class collision_detection:
             return True
         else:
             return False
+
+    # Collision handling
+
+    def handle_collision(self, collision):
+        relative_velocity = collision.get_relative_velocity()
+
+        velocity_along_normal = \
+            collision.get_velocity_along_normal(relative_velocity)
+
+        if velocity_along_normal > 0:
+            return
+
+        restitution = collision.get_restitution()
+
+        # Get impulse scalar
+        scalar = collision.get_impulse_scalar(restitution,
+                                              velocity_along_normal)
+
+        # Apply impulse, update object velocity
+        impulse = collision.get_impulse(scalar)
+        collision.update_velocity(impulse)
+
+    def positional_correction(self, collision):
+        correction_percent = 0.2
+        threshold = 0.1
+
+        if collision.penetration > threshold:
+            correction_factor = collision\
+                .get_positional_correction_factor(
+                    threshold, correction_percent)
+
+            collision.apply_correction_factor(correction_factor)
+
+class Collision:
+
+    def __init__(self, obj_a, obj_b, penetration, normal):
+        self.obj_a = obj_a
+        self.obj_b = obj_b
+        self.penetration = penetration
+        self.normal = normal
+
+    def get_relative_velocity(self):
+        return self.obj_b.velocity - self.obj_a.velocity
+
+    def get_velocity_along_normal(self, relative_velocity):
+        return relative_velocity.dot_product(self.normal)
+
+    def get_restitution(self):
+        return min(self.obj_a.restitution, self.obj_b.restitution)
+
+    def get_impulse_scalar(self, restitution, velocity_along_normal):
+        scalar = -(1 + restitution) * velocity_along_normal
+        scalar /= self.obj_a.mass_inv + self.obj_b.mass_inv
+        return scalar
+
+    def get_impulse(self, impulse_scalar):
+        return impulse_scalar * self.normal
+
+    def update_velocity(self, impulse):
+        self.obj_a.velocity -= self.obj_a.mass_inv * impulse
+        self.obj_b.velocity += self.obj_b.mass_inv * impulse
+
+    def get_positional_correction_factor(self, threshold, correction_percent):
+        return (self.penetration - threshold) / \
+                (self.obj_a.mass_inv + self.obj_b.mass_inv) * \
+                correction_percent * self.normal
+
+    def apply_correction_factor(self, correction_factor):
+        self.obj_a.add_to_position(-self.obj_a.inv_mass * correction_factor)
+        self.obj_b.add_to_position(self.obj_b.inv_mass * correction_factor)
