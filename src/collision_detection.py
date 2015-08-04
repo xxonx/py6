f94bc2db9bc927cc4ae6cf8b901c1f6beae41bfe
Matthias Wagner <xon.wagner@yahoo.de>
2015-08-04 20:42:42 +0200

    restructured collision detection algorithm

--- src/collision_detection.py
+++ src/collision_detection.py
@@ -17,53 +17,22 @@ __status__ = "Development"
 
 class CollisionDetection:
 
-    # Collision
     def detect_and_handle_collision(self, obj_a, obj_b):
-        if(self._is_collision(obj_a, obj_b)):
-            self._handle_collision(obj_a, obj_b)
-
-    # Collision detection
-
-    def _is_collision(self, obj_a, obj_b):
         if self._is_box(obj_a):
             if self._is_box(obj_b):
-                return self._is_collision_box_vs_box(obj_a, obj_b)
+                self._detect_collision_box_vs_box(obj_a, obj_b)
+
             elif self._is_circle(obj_b):
-                return self._is_collision_box_vs_circle(obj_a, obj_b)
+                self._detect_collision_box_vs_circle(obj_a, obj_b)
+
         elif self._is_circle(obj_a):
             if self._is_box(obj_b):
-                return self._is_collision_box_vs_circle(obj_b, obj_a)
+                self._detect_collision_box_vs_circle(obj_b, obj_a)
+
             elif self._is_circle(obj_b):
-                return self._is_collision_circle_vs_circle(obj_a, obj_b)
+                self._detect_collision_circle_vs_circle(obj_a, obj_b)
 
-        return False
-
-    def _is_collision_box_vs_box(self, box_a, box_b):
-        # Look for separating axis
-        if box_a.pos2.x < box_b.pos1.x or box_a.pos1.x > box_b.pos2.x:
-            return False
-        if box_a.pos2.y < box_b.pos1.y or box_a.pos1.y > box_b.pos2.y:
-            return False
-
-        # No separating axis found -> collision
-        return True
-
-    def _is_collision_circle_vs_circle(self, circle_a, circle_b):
-        radii_sum = circle_a.radius + circle_b.radius
-        radii_sum_squared = radii_sum * radii_sum
-        distance_squared = (circle_a.pos.x - circle_b.pos.x)**2 + \
-            (circle_a.pos.y - circle_b.pos.y)**2
-
-        # If the distance between the two center points is smaller than
-        # the sum of the two radii -> collision
-        return radii_sum_squared > distance_squared
-
-    def _is_collision_box_vs_circle(self, box, circle):
-        # TODO: implement box vs circle collision detection
-
-        return False
-
-    # Class checking
+    # Type checking
     def _is_box(self, obj):
         if isinstance(obj, Box):
             return True
@@ -76,13 +45,41 @@ class CollisionDetection:
         else:
             return False
 
+    # Collision detection
+    def _detect_collision_box_vs_box(self, box_a, box_b):
+        # Look for separating axis
+        if box_a.pos2.x < box_b.pos1.x or box_a.pos1.x > box_b.pos2.x:
+            return
+        if box_a.pos2.y < box_b.pos1.y or box_a.pos1.y > box_b.pos2.y:
+            return
+
+        # No separating axis found -> collision
+        # TODO: calculate normal and penetration
+        normal = 0
+        penetration = 0
+        self._handle_collision(box_a, box_b, normal, penetration)
+
+    def _detect_collision_circle_vs_circle(self, circle_a, circle_b):
+        radii_sum = circle_a.radius + circle_b.radius
+        radii_sum_squared = radii_sum * radii_sum
+        distance_squared = (circle_a.pos.x - circle_b.pos.x)**2 + \
+            (circle_a.pos.y - circle_b.pos.y)**2
+
+        # If the distance between the two center points is smaller than
+        # the sum of the two radii -> collision
+        if distance_squared < radii_sum_squared:
+            # Calculate normal and penetration depth
+            normal = circle_a.pos - circle_b.pos
+            penetration = radii_sum - distance_squared**0.5
+
+            self._handle_collision(circle_a, circle_b, normal, penetration)
+
+    def _detect_collision_box_vs_circle(self, box, circle):
+        # TODO: implement box vs circle collision detection
+        pass
+
     # Collision handling
-
-    def _handle_collision(self, obj_a, obj_b):
-        normal = self._get_collision_normal(obj_a, obj_b)
-        # TODO: implement penetration
-        # penetration = self._get_collision_penetration(obj_a, obj_b)
-
+    def _handle_collision(self, obj_a, obj_b, normal, penetration):
         # Calculate the objects velocity along the normal
         obj_a.velocity_on_normal = obj_a.velocity.project_onto(normal)
         obj_b.velocity_on_normal = obj_b.velocity.project_onto(normal)
@@ -90,13 +87,6 @@ class CollisionDetection:
         # Calculate new velocities
         obj_a.velocity = self._update_velocity(obj_a, obj_b)
         obj_b.velocity = self._update_velocity(obj_b, obj_a)
-
-    def _get_collision_normal(self, obj_a, obj_b):
-        # TODO: calculate normal
-        return obj_a.pos - obj_b.pos
-
-    def _get_collision_penetration(self, obj_a, obj_b):
-        pass  # TODO: calculate penetration
 
     def _update_velocity(self, obj_a, obj_b):
         relative_velocity_on_normal = \
